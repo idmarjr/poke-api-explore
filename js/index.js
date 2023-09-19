c34fe68a968571c77507abe2fc944ddc4592d5f6
Idmar Ramos Junior (Id) <idmar.ramos@booking.com>
2023-09-19 11:12:43 +0200

    Load more itens option
    - Create fetchNextData()
    - Add Load more button

--- js/index.js
+++ js/index.js
@@ -3,37 +3,40 @@
 // npx http-server
 // https://devdocs.io/dom/fetch
 console.log('Gotta catch \'em all!');
-const url = "https://pokeapi.co/api/v2/pokemon"
+const apiUrl = "https://pokeapi.co/api/v2/pokemon"
 
 const mainHeader = document.querySelector(".main-header");
 const pokeList = document.querySelector(".poke-list");
 const mainPagination = document.querySelector(".main-pagination");
 
-// Fetch data from API
-const fetchFromAPI = fetch(url);
+// Pagination variables
+let paginationNext;
+let paginationPrevious;
 
 // Handle promisse from API fetch
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-const jsonParsing = fetchFromAPI
-    // If the API response  is good, handle it as JSON
-    .then(function(response) {
-        return response.json();
-    })
-    // If something goes wrong, show console error message
-    .catch(function(err) {
-        console.error('Something went wrong when reaching the API: \n' + err);
-    });
+function fetchNextData (url){
 
-// Since .json() returns a promisse, we need to handle with it again
-jsonParsing
-    // What to do in case of success
-    .then(function(pokemonListJSON) {
-        renderPokemonList(pokemonListJSON.results);
-    })
-    // What to do if te data was not JSON: Show console error message
-    .catch(function(err) {
-        console.error('Data from API is not JSON: \n' + err);
-    });
+    fetch(url) 
+        // If the API response  is good, handle it as JSON
+        .then(function(response) {
+            return response.json();
+        })
+        // If something goes wrong, show console error message
+        .catch(function(err) {
+            console.error('Something went wrong when reaching the API: \n' + err);
+        })
+        // Since .json() returns a promisse, we need to handle with it again
+        .then(function(pokemonListJSON) {
+            renderPokemonList(pokemonListJSON.results);
+            paginationNext = pokemonListJSON.next;
+            paginationPrevious = pokemonListJSON.previous; 
+        })
+        // What to do if te data was not JSON: Show console error message
+        .catch(function(err) {
+            console.error('Data from API is not JSON: \n' + err);
+        });
+}
 
 // Template to render data from API
 function pokemonItemTemplate(pokemon) {
@@ -69,14 +72,23 @@ function renderPokemonList(pokemonArray) {
     });
 
     // Append the created list to the Dom.
-    pokeList.insertAdjacentHTML("afterbegin", allPokemonsList)
+    pokeList.insertAdjacentHTML("beforeend", allPokemonsList)
 };
 
 // Load more
 function renderLoadMore() {
-    const template =`<button>Load more</button>`
-    mainPagination.insertAdjacentHTML("afterbegin", template)
+    const template =`<button id="load-more">Load more</button>`
+    mainPagination.insertAdjacentHTML("afterbegin", template);
+
+    const loadMoreButton = document.getElementById("load-more");
+    loadMoreButton.addEventListener("click", function(){
+        fetchNextData(paginationNext)
+    })
+
 };
+
+// Populate page on onload
+fetchNextData(apiUrl);
 
 // Render pagination content in the page
 renderLoadMore();
